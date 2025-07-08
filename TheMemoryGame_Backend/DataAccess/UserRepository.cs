62b92f6abc68e69dbb08f37e1164d4b6ecd969c9
coderbang-bang <E1509801@u.nus.edu>
2025-07-08 22:14:13 +0800

    feat: add user id to session upon login + refactoring

--- TheMemoryGame_Backend/DataAccess/UserRepository.cs
+++ TheMemoryGame_Backend/DataAccess/UserRepository.cs
@@ -1,39 +1,22 @@
-﻿
-using MySqlConnector;
+﻿using Microsoft.EntityFrameworkCore;
 using TheMemoryGame_Backend.Models;
 
 namespace TheMemoryGame_Backend.DataAccess
 {
     public class UserRepository
     {
-        private readonly DatabaseHelper _dbHelper;
+        private readonly MyDbContext _dbContext;
 
-        public UserRepository(DatabaseHelper dbHelper)
+        public UserRepository(MyDbContext dbContext)
         {
-            _dbHelper = dbHelper;
+            _dbContext = dbContext;
         }
 
         public User? GetUserByUsername(string username)
         {
-            using (var connection = new MySqlConnection(_dbHelper.GetConnectionString()))
-            {
-                connection.Open();
-                var command = new MySqlCommand("SELECT * FROM User WHERE Username = @Username", connection);
-                command.Parameters.AddWithValue("@Username", username);
-                using (var reader = command.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        return new User
-                        {
-                            Id = reader.GetString("Id"),
-                            Username = reader.GetString("Username"),
-                            PasswordHash = reader.GetString("PasswordHash")
-                        };
-                    }
-                }
-            }
-            return null;
+            return _dbContext.User
+                .AsNoTracking()
+                .FirstOrDefault(u => u.Username == username);
         }
     }
 }

20e39037432127d2fccf9e7a38a6d0ae76903c8d
Ginger-ui <E1511449@u.nus.edu>
2025-07-08 22:24:28 +0800

    add the music and music
    # Conflicts:
    #	TheMemoryGame_Android/app/src/main/res/values/strings.xml
    #	TheMemoryGame_Backend/Models/MyDbContext.cs
    #	TheMemoryGame_Backend/Program.cs

--- TheMemoryGame_Backend/Models/MyDbContext.cs
+++ TheMemoryGame_Backend/Models/MyDbContext.cs
@@ -3,33 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace TheMemoryGame_Backend.Models
 {
     public class MyDbContext : DbContext
     {
         public MyDbContext() { }
-
-        private readonly IConfiguration _configuration;
-        public MyDbContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                var connectionString = _configuration.GetConnectionString("DefaultConnection");
-                optionsBuilder.UseMySql(
-                    connectionString,
-                    new MySqlServerVersion(new Version(8, 0, 41))
-                );
-                optionsBuilder.UseLazyLoadingProxies();
-            }
+            optionsBuilder.UseMySql(
+                // Provide database connection string
+                "server=localhost;user=root;password=password;database=TheMemoryGame;",
+                new MySqlServerVersion(new Version(8, 0, 41))
+            );
+            optionsBuilder.UseLazyLoadingProxies();
         }
-
         // Our database tables
         public DbSet<User> User { get; set; }
         public DbSet<Record> Record { get; set; }
